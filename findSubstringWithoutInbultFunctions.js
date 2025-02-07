04aa7cf0c85f18c9c2502d4d4f517a36d16f7667
Sivaram Anandkumar <52231444+sivaramanand@users.noreply.github.com>
2025-02-07 19:02:26 +0530

    Create findSubstringWithoutInbultFunctions.js

---
+++ findSubstringWithoutInbultFunctions.js
@@ -0,0 +1,22 @@
+function findsubstring(mainstr, substr) {
+  const sublength = substr.length;
+  var flag = 0;
+  var minstr = '';
+  for (var i = 0; i < mainstr.length; i++) {
+    for (var j = 0; j < sublength; j++) {
+      var minstr = minstr + mainstr[i + j];
+    }
+    if (minstr === substr) {
+      console.log('this is a substring');
+      flag = flag + 1;
+      break;
+    } else {
+      minstr = '';
+    }
+  }
+  if (flag === 0) {
+    console.log('no substring is contained here');
+  }
+}
+
+findsubstring('asdfghjkl', 'hjl');
